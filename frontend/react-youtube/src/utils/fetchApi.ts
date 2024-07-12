af9233fbf2348d8866f1cbf162c3b1c6f295d35a
Francis7575 <francis.albert.2009@gmail.com>
2024-07-12 16:41:08 -0700

    updated

--- frontend/react-youtube/src/utils/fetchApi.ts
+++ frontend/react-youtube/src/utils/fetchApi.ts
@@ -2,7 +2,7 @@ const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
     headers: {
-        'X-RapidAPI-Key': import.meta.env.VITE_REACT_RAPID_API_KEY!,
+        'X-RapidAPI-Key': import.meta.env.VITE_REACT_RAPID_API_KEY,
         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
 };
