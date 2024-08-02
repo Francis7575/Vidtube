8fc169358af433186d6d844d63dc522873ea751c
Francis7575 <francis.albert.2009@gmail.com>
2024-08-01 17:52:17 -0700

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
 import { useAuth } from './hooks/useAuth'
-import { Login, Signup, Forgotpassword, VideoDetail, ChannelDetail, SearchFeed } from './components'
+import { Login, Signup, VideoDetail, ChannelDetail, SearchFeed } from './components'
 import { LoginData } from './types/types'
 
 const App = () => {
@@ -16,7 +16,6 @@ const App = () => {
       <Route path="/video/:id" element={<VideoDetail />} />
       <Route path="/channel/:id" element={<ChannelDetail />} />
       <Route path="/login" element={<Login onLogin={handleLoginWrapper} />} />
-      <Route path="/forgot-password" element={<Forgotpassword />} />
       <Route path="/signup" element={<Signup onSignup={handleSignup} />} />
       <Route path="/search/:searchTerm" element={<SearchFeed />} />
     </Routes>
